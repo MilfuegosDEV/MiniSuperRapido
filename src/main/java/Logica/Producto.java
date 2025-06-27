97ccc8e17fe8c15161a93caf190302a9160b4d91
Juan Daniel Luna Cienfuegos <juandaniel.lunacienfuegos@gmail.com>
2025-06-27 17:17:39 -0600

    Initial Commit

--- src/main/java/Logica/Producto.java
+++ src/main/java/Logica/Producto.java
@@ -8,45 +8,25 @@ package Logica;
  *
  * @author daniel
  */
-public abstract class Producto {
-    private int cantidad;
-    private String nombre;
-    private String codigo;
-    private double precio;
-    
-    public Producto() {
+public class Producto extends ProductoBase {
+    public Producto () {
         
     }
-    
-    public Producto( String codigo, String nombre, int cantidad, double precio) {
-        this.codigo = codigo;
-        this.nombre = nombre;
-        this.cantidad = cantidad;
-        this.precio = precio;
-    }
-    public String getNombre() {
-        return this.nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public Producto(String codigo, String nombre, int cantidad, double precio) {
+        super(codigo, nombre, cantidad, precio);
     }
     
-    public String getCodigo ( ) {
-        return this.codigo;
+    @Override
+    public double calcularPrecio() {
+        return this.getPrecio() * this.getCantidad();
     }
     
-    public int getCantidad () {
-        return this.cantidad;
+    @Override
+    public void Mostrar() {
+        System.out.printf(
+            "Código: %s, Nombre: %s, Cantidad: %d, Precio unitario: %.2f, Subtotal: %.2f%n",
+            this.getCodigo(), this.getNombre(), this.getCantidad(), this.getPrecio(), this.calcularPrecio()
+        );
     }
-    public void setCantidad(int cantidad) {
-        this.cantidad = cantidad;
-    }
-    public double getPrecio() {
-        return this.precio;
-    }
-    public void setPrecio(double precio) {
-        this.precio = precio;
-    }
-    public abstract void Mostrar();
-    public abstract double calcularPrecio();
+
 }
